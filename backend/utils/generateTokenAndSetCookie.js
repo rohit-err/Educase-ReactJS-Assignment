b272fd3a54abf246cfde0482d515af4005b6aeec
Rohit Kumar <rohit2004r82@gmail.com>
2025-08-27 18:25:30 +0530

    fixed cookie

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -7,11 +7,12 @@ const generateToken = (res, userId) => {
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production", 
+        sameSite: "none",  
         maxAge: 7 * 24 * 60 * 60 * 1000,
     })
+
     return token
 }
 
-module.exports = generateToken;
+module.exports = generateToken
