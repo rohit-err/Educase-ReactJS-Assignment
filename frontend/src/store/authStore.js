de0edb137d63773fbcbaa726adc61936fcb7f91c
Rohit Kumar <rohit2004r82@gmail.com>
2025-08-27 17:52:28 +0530

    updated backend url

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand"
 import axios from "axios"
 
-const API_URL = "http://localhost:8000/api"
+const API_URL = "https://educase-reactjs-assignment-backend.onrender.com/api"
 axios.defaults.withCredentials = true
 
 const useAuthStore = create((set) => ({
